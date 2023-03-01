ef4216a892c991471869e74d53f9f6aacf4270c9
Jonathan Molero <110490630+molerojj@users.noreply.github.com>
2023-02-28 23:28:07 -0400

    new backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Landing, Detail, Home, Form } from './views'
 import Page404 from './views/Page404/Page404'
 import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3001/';
+axios.defaults.baseURL = 'deploy-videogames-project-production.up.railway.app/';
 
 
 function App() {
